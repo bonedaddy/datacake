912724eebd12d867630a051d5921b6e1005ac501
bonedaddy <boned4ddy@protonmail.com>
2022-12-14 21:52:02 -0800

    add 2 node cluster test for sled

--- datacake-sled/tests/basic_cluster.rs
+++ datacake-sled/tests/basic_cluster.rs
@@ -14,56 +14,83 @@ static KEYSPACE: &str = "sqlite-store";
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_basic_sled_cluster() -> Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_LOG", "debug,h2=info,tower=info,sled=info");
     let _ = tracing_subscriber::fmt::try_init();
 
-    let store = SledStorage::open_temporary()?;
+    let store_1 = SledStorage::open_temporary()?;
+    let store_2 = SledStorage::open_temporary()?;
 
-    let addr = "127.0.0.1:9000".parse::<SocketAddr>().unwrap();
-    let connection_cfg = ConnectionConfig::new(addr, addr, Vec::<String>::new());
+    let addr_1 = "127.0.0.1:9000".parse::<SocketAddr>().unwrap();
+    let connection_cfg_1 = ConnectionConfig::new(addr_1, addr_1, Vec::<String>::new());
 
-    let cluster = DatacakeCluster::connect(
+    let addr_2 = "127.0.0.1:9001".parse::<SocketAddr>().unwrap();
+    let connection_cfg_2 = ConnectionConfig::new(addr_2, addr_2, vec!["127.0.0.1:9000".to_string()]);
+
+    let cluster_1 = DatacakeCluster::connect(
         "node-1",
-        connection_cfg,
-        store,
+        connection_cfg_1,
+        store_1,
         DCAwareSelector::default(),
         ClusterOptions::default(),
     )
     .await?;
 
-    let handle = cluster.handle();
+    let cluster_2 = DatacakeCluster::connect(
+        "node-2",
+        connection_cfg_2,
+        store_2,
+        DCAwareSelector::default(),
+        ClusterOptions::default(),
+    )
+    .await?;
 
-    handle
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+    let handle_1 = cluster_1.handle();
+    let handle_2 = cluster_2.handle();
+
+    handle_1
         .put(KEYSPACE, 1, b"Hello, world".to_vec(), Consistency::EachQuorum)
         .await
         .expect("Put value.");
 
-    let doc = handle
+    
+
+    let doc = handle_1
         .get(KEYSPACE, 1)
         .await
         .expect("Get value.")
         .expect("Document should not be none");
     assert_eq!(doc.id, 1);
     assert_eq!(doc.data.as_ref(), b"Hello, world");
-    log::info!("deleting key");
-    handle
+
+    let doc = handle_2
+        .get(KEYSPACE, 1)
+        .await
+        .expect("Get value.")
+        .expect("Document should not be none");
+    assert_eq!(doc.id, 1);
+    assert_eq!(doc.data.as_ref(), b"Hello, world");
+
+
+    handle_2
         .del(KEYSPACE, 1, Consistency::EachQuorum)
         .await
         .expect("Del value.");
-    log::info!("getting key");
-    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-    let doc = handle.get(KEYSPACE, 1).await.expect("Get value.");
-    log::info!("got doc {:#?}", doc);
+
+    let doc = handle_1.get(KEYSPACE, 1).await.expect("Get value.");
+
     assert!(doc.is_none(), "No document should not exist!");
 
-    handle
+    handle_1
         .del(KEYSPACE, 2, Consistency::EachQuorum)
         .await
         .expect("Del value which doesnt exist locally.");
-    let doc = handle.get(KEYSPACE, 2).await.expect("Get value.");
+    let doc = handle_1.get(KEYSPACE, 2).await.expect("Get value.");
     assert!(doc.is_none(), "No document should not exist!");
 
-    cluster.shutdown().await;
+    cluster_1.shutdown().await;
+    cluster_2.shutdown().await;
 
     Ok(())
 }
